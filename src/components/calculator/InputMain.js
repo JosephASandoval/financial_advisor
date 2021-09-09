01fa8836f562ecbd822a3da16c4c7f06db1e61d8
JosephASandoval <josephasandoval17@gmail.com>
2021-09-08 23:44:23 -0700

    added styling to difference and new amount columns

--- src/components/calculator/InputMain.js
+++ src/components/calculator/InputMain.js
@@ -1,9 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import { Context } from "../../Context";
 
 const InputMain = () => {
   const { currentAmount, setCurrentAmount, newAmount, difference } =
     useContext(Context);
+
+  const bondsDiff = useRef();
+  const largeCapDiff = useRef();
+  const midCapDiff = useRef();
+  const foreignDiff = useRef();
+  const smallCapDiff = useRef();
 
   function handleCurrentAmount(e) {
     const { name, value } = e.target;
@@ -14,6 +20,69 @@ const InputMain = () => {
       };
     });
   }
+
+  useEffect(() => {
+    if (bondsDiff.current.value) {
+      const { value } = bondsDiff.current;
+      switch (value < 0) {
+        case true:
+          bondsDiff.current.className = "risk-calculator-main-diff-neg";
+          break;
+        case false:
+          bondsDiff.current.className = "risk-calculator-main-diff-pos";
+          bondsDiff.current.value = "+" + bondsDiff.current.value;
+          break;
+      }
+    }
+    if (largeCapDiff.current.value) {
+      const { value } = largeCapDiff.current;
+      switch (value < 0) {
+        case true:
+          largeCapDiff.current.className = "risk-calculator-main-diff-neg";
+          break;
+        case false:
+          largeCapDiff.current.className = "risk-calculator-main-diff-pos";
+          largeCapDiff.current.value = "+" + largeCapDiff.current.value;
+          break;
+      }
+    }
+    if (midCapDiff.current.value) {
+      const { value } = midCapDiff.current;
+      switch (value < 0) {
+        case true:
+          midCapDiff.current.className = "risk-calculator-main-diff-neg";
+          break;
+        case false:
+          midCapDiff.current.className = "risk-calculator-main-diff-pos";
+          midCapDiff.current.value = "+" + midCapDiff.current.value;
+          break;
+      }
+    }
+    if (foreignDiff.current.value) {
+      const { value } = foreignDiff.current;
+      switch (value < 0) {
+        case true:
+          foreignDiff.current.className = "risk-calculator-main-diff-neg";
+          break;
+        case false:
+          foreignDiff.current.className = "risk-calculator-main-diff-pos";
+          foreignDiff.current.value = "+" + foreignDiff.current.value;
+          break;
+      }
+    }
+    if (smallCapDiff.current.value) {
+      const { value } = smallCapDiff.current;
+      switch (value < 0) {
+        case true:
+          smallCapDiff.current.className = "risk-calculator-main-diff-neg";
+          break;
+        case false:
+          smallCapDiff.current.className = "risk-calculator-main-diff-pos";
+          smallCapDiff.current.value = "+" + smallCapDiff.current.value;
+          break;
+      }
+    }
+  });
 
   return (
     <div className="risk-calculator-main">
@@ -26,8 +95,21 @@ const InputMain = () => {
             value={currentAmount.bonds}
             onChange={handleCurrentAmount}
           />
-          <input type="text" name="bonds" value={difference.bonds} disabled />
-          <input type="text" name="bonds" value={newAmount.bonds} disabled />
+          <input
+            className=""
+            ref={bondsDiff}
+            type="text"
+            name="bonds"
+            value={difference.bonds}
+            disabled
+          />
+          <input
+            className="risk-calculator-main-new"
+            type="text"
+            name="bonds"
+            value={newAmount.bonds}
+            disabled
+          />
         </div>
       </div>
       <div className="risk-calculator-main-row">
@@ -40,12 +122,15 @@ const InputMain = () => {
             onChange={handleCurrentAmount}
           />
           <input
+            className=""
+            ref={largeCapDiff}
             type="text"
             name="largeCap"
             value={difference.largeCap}
             disabled
           />
           <input
+            className="risk-calculator-main-new"
             type="text"
             name="largeCap"
             value={newAmount.largeCap}
@@ -62,8 +147,21 @@ const InputMain = () => {
             value={currentAmount.midCap}
             onChange={handleCurrentAmount}
           />
-          <input type="text" name="midCap" value={difference.midCap} disabled />
-          <input type="text" name="midCap" value={newAmount.midCap} disabled />
+          <input
+            className=""
+            ref={midCapDiff}
+            type="text"
+            name="midCap"
+            value={difference.midCap}
+            disabled
+          />
+          <input
+            className="risk-calculator-main-new"
+            type="text"
+            name="midCap"
+            value={newAmount.midCap}
+            disabled
+          />
         </div>
       </div>
       <div className="risk-calculator-main-row">
@@ -76,12 +174,15 @@ const InputMain = () => {
             onChange={handleCurrentAmount}
           />
           <input
+            className=""
+            ref={foreignDiff}
             type="text"
             name="foreign"
             value={difference.foreign}
             disabled
           />
           <input
+            className="risk-calculator-main-new"
             type="text"
             name="foreign"
             value={newAmount.foreign}
@@ -99,12 +200,15 @@ const InputMain = () => {
             onChange={handleCurrentAmount}
           />
           <input
+            className=""
+            ref={smallCapDiff}
             type="text"
             name="smallCap"
             value={difference.smallCap}
             disabled
           />
           <input
+            className="risk-calculator-main-new"
             type="text"
             name="smallCap"
             value={newAmount.smallCap}
